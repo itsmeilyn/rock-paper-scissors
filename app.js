70a2dbddfe9c597f455704830c3e34587c7e0376
meilyn <119741026+itsmeilyn@users.noreply.github.com>
2026-07-30 07:18:38 +0700

    fix: getComputerChoice() didn't return any value

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ function getComputerChoice() {
   const comChoice = Math.floor(Math.random() * 3); // generate random number 0-2
   switch (comChoice) {
     case 0:
-      console.log("rock");
-      break;
+      return "rock";
     case 1:
-      console.log("paper");
-      break;
+      return "paper";
     case 2:
-      console.log("scissors");
-      break;
+      return "scissors";
     default:
       console.log("invalid");
   }
 }
+
+const computerSelection = getComputerChoice();
