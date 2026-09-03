4c4401e79059cab0067a7f559351f5cf87dafdf2
meilyn <119741026+itsmeilyn@users.noreply.github.com>
2026-09-03 20:24:07 +0700

    add react component for rps

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
-export default function App() {
-  return <h1>Hello, world</h1>;
+function App() {
+  return (
+    <div className="card">
+      <h1>Rock Paper Scissors</h1>
+      <div className="tabButtons">
+        <button>Rock</button>
+        <button>Paper</button>
+        <button>Scissors</button>
+      </div>
+      <div className="display">
+        <h2>Scoreboard</h2>
+        <p>Round:</p>
+        <p>YOU:</p>
+        <p>COMPUTER:</p>
+        <p>RESULT:</p>
+        <p></p>
+      </div>
+      <button id="btnReset">Reset</button>
+    </div>
+  );
 }
+
+export default App;
