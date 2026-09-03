afc271b682fe3fb81c2ad09d44e124dc592278bf
meilyn <119741026+itsmeilyn@users.noreply.github.com>
2026-09-03 14:38:22 +0700

    Refactor UI layout and add reset functionality for the game

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+// DOM
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const reset = document.querySelector("#btnReset");
+const playerDisplayScore = document.querySelector("#playerScore");
+const comDisplayScore = document.querySelector("#computerScore");
+const result = document.querySelector("#result");
+const roundDisplay = document.querySelector("#round");
+
 function getComputerChoice() {
   const comChoice = Math.floor(Math.random() * 3); // generate random number 0-2
   switch (comChoice) {
@@ -14,37 +24,73 @@ function getComputerChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
-let total_games = 0;
+let round = 0;
+const MAX_ROUNDS = 5;
+let isGameOver = false;
 
 function playRound(humanChoice, computerChoice) {
-  if(total_games !== 5) {
+  if (isGameOver) return;
+  if (round !== MAX_ROUNDS) {
     if (
-        (humanChoice === "rock" && computerChoice === "scissors") ||
-        (humanChoice === "paper" && computerChoice === "rock") ||
-        (humanChoice === "scissors" && computerChoice === "paper")
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "scissors" && computerChoice === "paper")
     ) {
       humanScore++;
-      total_games++;
-
-      console.log(`You win! you:${humanChoice} beats com:${computerChoice}`);
+      round++;
+      updateScoreboard();
+      result.textContent = `You won this round! ${humanChoice} beats ${computerChoice}`;
     } else if (humanChoice === computerChoice) {
-      total_games++;
-      console.log(`"Tie! you:${humanChoice}, com:${computerChoice}`);
+      round++;
+      updateScoreboard();
+      result.textContent = "Tie!";
     } else {
       computerScore++;
-      total_games++
-      console.log(`You lose! com:${computerChoice} beats you:${humanChoice}`);
+      round++;
+      updateScoreboard();
+      result.textContent = `Computer won this round! ${computerChoice} beats ${humanChoice}`;
     }
   }
+  if (round === MAX_ROUNDS) {
+    isGameOver = true;
+    gameOver();
+  }
+}
 
+function updateScoreboard() {
+  roundDisplay.textContent = round;
+  playerDisplayScore.textContent = humanScore;
+  comDisplayScore.textContent = computerScore;
+}
+
+function gameOver() {
+  if (humanScore > computerScore) {
+    result.textContent = "GAME OVER. YOU WON!";
+  } else if (computerScore > humanScore) {
+    result.textContent = "GAME OVER. COM WON!";
+  } else {
+    result.textContent = "GAME OVER. TIE!";
+  }
+  rock.disabled = true;
+  paper.disabled = true;
+  scissors.disabled = true;
+}
+
+function resetGame() {
+  round = 0;
+  humanScore = 0;
+  computerScore = 0;
+  isGameOver = false;
+  playerDisplayScore.textContent = "0";
+  result.textContent = "-";
+  comDisplayScore.textContent = "";
+  roundDisplay.textContent = "0";
+  rock.disabled = false;
+  paper.disabled = false;
+  scissors.disabled = false;
 }
 
 // console.log(`Game over! human:${humanScore} computer:${computerScore}`);
-
-// DOM
-const rock = document.querySelector("#rock");
-const paper = document.querySelector("#paper");
-const scissors = document.querySelector("#scissors");
 
 rock.addEventListener("click", () => {
   playRound("rock", getComputerChoice());
@@ -56,4 +102,8 @@ paper.addEventListener("click", () => {
 
 scissors.addEventListener("click", () => {
   playRound("scissors", getComputerChoice());
+});
+
+reset.addEventListener("click", () => {
+  resetGame();
 });
